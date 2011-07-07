b20b29403bc1c85ad0c9cfdcaeca615050a664fb
Trevor_Umeda <trevor.umeda@gmail.com>
2011-07-07 10:52:38 -0700

    user signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
    def self.authenticate(email,submitted_password)
      user = find_by_email(email)
      return nil if user.nil?
-     return user if user.has_password?(submitted_professor)
+     return user if user.has_password?(submitted_password)
    end
    
    private
